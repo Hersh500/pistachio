89ac85e2bb790bf3d019b72f72fca98c4b898d26
Hersh Sanghvi <hersh500@gmail.com>
2015-03-30 11:43:19 -0700

    List Mailboxes

--- pistachio.py
+++ pistachio.py
@@ -75,24 +75,35 @@ def process_mailbox(IMAP_SERVER):
 		msg = email.message_from_string(data[0][1])
 
 def get_mailboxes(IMAP_SERVER):
-	mailboxes = IMAP_SERVER.list()	
-
-def get_mails_since_date(IMAP_SERVER):
+	rv, mailboxes = IMAP_SERVER.list()	
+	for mailbox in mailboxes: 
+		lst = mailbox.strip('"').split()
+		print(lst)
+	"""
+		if len(lst) > 3:
+			print lst[2].strip('"') + " " + lst[3]
+		else:
+			print lst[2]
+	"""
+def get_mails_since_date(IMAP_SERVER, date):
 	#assume mailbox already selected
-	months = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr", "05":"May", "06":"Jun", "07":"Jul", "08":"Aug", "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
+	pass
 
-def get_mails_on_date(IMAP_SERVER):
+def get_mails_on_date(IMAP_SERVER, date):
 	#assume mailbox already selected
+	#rv, ids = IMAP_SERVER.search(None, "(SINCE" + ) 
+	pass
 
 def main():
 	global results
+#	global months = {"01":"Jan", "02":"Feb", "03":"Mar", "04":"Apr", "05":"May", "06":"Jun", "07":"Jul", "08":"Aug", "09":"Sep", "10":"Oct", "11":"Nov", "12":"Dec"}
 	parser = argparse.ArgumentParser(description='Simple Terminal mail client')
 	
 	parser.add_argument("-m", action='store', dest='mailbox', help='Specify a mailbox', default="INBOX")
 	parser.add_argument("-d", action='store', dest='date', help='Specify a date in the form of dd-mon-yyyy')
 	parser.add_argument("-c", action='store_true', dest='config', help="change the username/password")
 	parser.add_argument("-g", action='store', dest="mail_id", help="get a specific message based on its id", type=int)
-	parser.add_argument("-s", action-'store', dest='keyword', help='search for a message in a mailbox based on a keyword', type=int)
+	parser.add_argument("-s", action='store', dest='keyword', help='search for a message in a mailbox based on a keyword', type=int)
 	parser.add_argument("--od", action='store', dest="on_date", help="search for a message on a specific date, see -d for format")
 	parser.add_argument("--sh", action="store_true", dest="act_shell", help="Add flag if you want to open shell", default=False)
 	parser.add_argument("--lm", action="store_true", dest="list_mails", help="Add flag if you want to see the mailboxes in your account", default=False)
@@ -100,7 +111,9 @@ def main():
 	results = parser.parse_args()
 	
 	IMAP_SERVER = initialize()
+	if results.list_mails:
+		get_mailboxes(IMAP_SERVER)
 	
-
+	IMAP_SERVER.logout()
 if __name__ == "__main__":
 	main()
